a37c27e59494a655572136de8d29ec42379cae9d
Will Brown <wtbrown10@gmail.com>
2020-05-20 00:30:49 -0400

    changed text with DOM through innerHTML

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const wmf = document.querySelector("#book-list li:nth-child(2) .name");
-// console.log(wmf);
-var books = document.querySelector("#book-list li .name");
-// console.log(books);
+var books = document.querySelectorAll("#book-list li .name");
 
-books = document.querySelectorAll("#book-list li .name");
-
-// console.log(books);
-
-Array.from(books).forEach(function (books) {
-  console.log(books);
+Array.from(books).forEach(function (book) {
+  book.textContent += " (Book Title)";
 });
+
+const booklist = document.querySelector("#book-list");
+// booklist.innerHTML = "<h2>Books and More Books</h2>";
+booklist.innerHTML += "<p>This is how you add HTML</p>";
