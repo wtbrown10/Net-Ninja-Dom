d26957399b15f77156d6c0910ad81d101c250970
Will Brown <wtbrown10@gmail.com>
2020-05-19 23:32:51 -0400

    added titles a foreach

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var banner = document.getElementById("page-banner");
-var booklist = document.getElementById("book-list");
+var titles = document.getElementsByClassName("title");
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function (item) {
+  console.log(item);
+});
