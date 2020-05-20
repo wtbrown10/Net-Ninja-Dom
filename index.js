a5295eb7838c331e5cfb4a62b92df9d61a176060
Will Brown <wtbrown10@gmail.com>
2020-05-19 23:47:52 -0400

    introduced querySelector

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var titles = document.getElementsByClassName("title");
+const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+// console.log(wmf);
+var books = document.querySelector("#book-list li .name");
+// console.log(books);
 
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
+books = document.querySelectorAll("#book-list li .name");
 
-Array.from(titles).forEach(function (item) {
-  console.log(item);
+// console.log(books);
+
+Array.from(books).forEach(function (books) {
+  console.log(books);
 });
