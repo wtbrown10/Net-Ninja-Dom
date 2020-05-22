9d668b9fd1bab8351e79f1e7a2875746bb8314c9
Will Brown <wtbrown10@gmail.com>
2020-05-22 18:23:46 -0400

    eventlistener

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const booklist = document.querySelector("#book-list");
+var btns = document.querySelectorAll("#book-list .delete");
 
-console.log("book-list next sibling is:", booklist.nextSibling);
-console.log("book-list next element sibling is:", booklist.nextElementSibling);
-console.log("book-list previous sibling is:", booklist.previousSibling);
-console.log(
-  "book-list previous element sibling is:",
-  booklist.previousElementSibling
-);
+Array.from(btns).forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const li = e.target.parentElement;
 
-booklist.previousElementSibling.querySelector("p").innerHTML +=
-  "<br/>Too cool for everyone else";
+    li.parentNode.removeChild(li);
+  });
+});
+
+const link = document.querySelector("#page-banner a");
+
+link.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("navigation to".e.target.textContent, "was prevented");
+});
