fa816292e00382ef477c6ee50a255d23cd4cc553
Will Brown <wtbrown10@gmail.com>
2020-05-22 19:52:54 -0400

    added class names

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ addForm.addEventListener("submit", function (e) {
   deleteBtn.textContent = "delete";
   bookName.textContent = value;
 
+  //add classes
+  bookName.classList.add("name");
+  deleteBtn.classList.add("delete");
+
   //append to document
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
