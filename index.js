b393076cd9c1c54491b36ba3549112b68fa9b3ed
Will Brown <wtbrown10@gmail.com>
2020-05-22 19:43:00 -0400

    created elements

--- index.js
+++ index.js
@@ -14,5 +14,18 @@ const addForm = document.forms["add-book"];
 addForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+
+  // create elements
+  const li = document.createElement("li");
+  const bookName = document.createElement("span");
+  const deleteBtn = document.createElement("span");
+
+  //add text content
+  deleteBtn.textContent = "delete";
+  bookName.textContent = value;
+
+  //append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
