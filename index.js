c0701a2e34be9921bec1abaf9170112f4e6bc504
Will Brown <wtbrown10@gmail.com>
2020-05-22 21:30:08 -0400

    added DOMContentLoaded

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
-const list = document.querySelector("#book-list ul");
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.querySelector("#book-list ul");
 
-list.addEventListener("click", function (e) {
-  if (e.target.className == "delete") {
-    const li = e.target.parentElement;
-    list.removeChild(li);
-  }
-});
-
-//add book list
-
-const addForm = document.forms["add-book"];
-
-addForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const value = addForm.querySelector('input[type="text"]').value;
-
-  // create elements
-  const li = document.createElement("li");
-  const bookName = document.createElement("span");
-  const deleteBtn = document.createElement("span");
-
-  //add text content
-  deleteBtn.textContent = "delete";
-  bookName.textContent = value;
-
-  //add  classes
-  bookName.classList.add("name");
-  deleteBtn.classList.add("delete");
-
-  //append to document
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
-
-//hide books
-const hideBox = document.querySelector("#hide");
-hideBox.addEventListener("change", function (e) {
-  if (hideBox.checked) {
-    list.style.display = "none";
-  } else {
-    list.style.display = "initial";
-  }
-});
-
-// filter books
-
-const searchBar = document.forms["search-books"].querySelector("input");
-searchBar.addEventListener("keyup", function (e) {
-  const term = e.target.value.toLowerCase();
-  const books = list.getElementsByTagName("li");
-  Array.from(books).forEach(function (book) {
-    const title = book.firstElementChild.textContent;
-    if (title.toLowerCase().indexOf(term) != -1) {
-      book.style.display = "block";
-    } else {
-      book.style.display = "none";
+  list.addEventListener("click", function (e) {
+    if (e.target.className == "delete") {
+      const li = e.target.parentElement;
+      list.removeChild(li);
     }
   });
-});
 
-// Tabbed content
-const tabs = document.querySelector(".tabs");
-const panels = document.querySelectorAll(".panel");
+  //add book list
 
-tabs.addEventListener("click", function (e) {
-  if (e.target.tagName == "LI") {
-    const targetPanel = document.querySelector(e.target.dataset.target);
-    panels.forEach(function (panel) {
-      if (panel == targetPanel) {
-        panel.classList.add("active");
+  const addForm = document.forms["add-book"];
+
+  addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    // create elements
+    const li = document.createElement("li");
+    const bookName = document.createElement("span");
+    const deleteBtn = document.createElement("span");
+
+    //add text content
+    deleteBtn.textContent = "delete";
+    bookName.textContent = value;
+
+    //add  classes
+    bookName.classList.add("name");
+    deleteBtn.classList.add("delete");
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+
+  //hide books
+  const hideBox = document.querySelector("#hide");
+  hideBox.addEventListener("change", function (e) {
+    if (hideBox.checked) {
+      list.style.display = "none";
+    } else {
+      list.style.display = "initial";
+    }
+  });
+
+  // filter books
+
+  const searchBar = document.forms["search-books"].querySelector("input");
+  searchBar.addEventListener("keyup", function (e) {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName("li");
+    Array.from(books).forEach(function (book) {
+      const title = book.firstElementChild.textContent;
+      if (title.toLowerCase().indexOf(term) != -1) {
+        book.style.display = "block";
       } else {
-        panel.classList.remove("active");
+        book.style.display = "none";
       }
     });
-  }
+  });
+
+  // Tabbed content
+  const tabs = document.querySelector(".tabs");
+  const panels = document.querySelectorAll(".panel");
+
+  tabs.addEventListener("click", function (e) {
+    if (e.target.tagName == "LI") {
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      panels.forEach(function (panel) {
+        if (panel == targetPanel) {
+          panel.classList.add("active");
+        } else {
+          panel.classList.remove("active");
+        }
+      });
+    }
+  });
 });
