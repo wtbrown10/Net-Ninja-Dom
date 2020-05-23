0fe25f29e131b77067587067e961a9fef2e657d5
Will Brown <wtbrown10@gmail.com>
2020-05-22 21:01:27 -0400

    add tabbed content

--- index.js
+++ index.js
@@ -59,3 +59,20 @@ searchBar.addEventListener("keyup", function (e) {
     }
   });
 });
+
+// Tabbed content
+const tabs = document.querySelector(".tabs");
+const panels = document.querySelectorAll("panel");
+
+tabs.addEventListener("click", function (e) {
+  if (e.target.tagName == "LI") {
+    const targetPanel = document.querySelector(e.target.dataSet.target);
+    panels.forEach(function (panel) {
+      if (panel == targetPanel) {
+        panel.classList.add("active");
+      } else {
+        panel.classList.remove("active");
+      }
+    });
+  }
+});
