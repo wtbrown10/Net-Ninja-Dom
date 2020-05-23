4e06c344be56719f16ff99aaec4f7d947d4d8bfe
Will Brown <wtbrown10@gmail.com>
2020-05-22 20:43:29 -0400

    added filter books

--- index.js
+++ index.js
@@ -43,3 +43,19 @@ hideBox.addEventListener("change", function (e) {
     list.style.display = "initial";
   }
 });
+
+// filter books
+
+const searchBar = document.forms["search-books"].querySelector("input");
+searchBar.addEventListener("keyup", function (e) {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName("li");
+  Array.from(books).forEach(function (book) {
+    const title = book.firstElementChild.textContent;
+    if (title.toLowerCase().indexOf(term) != -1) {
+      book.style.display = "block";
+    } else {
+      book.style.display = "none";
+    }
+  });
+});
